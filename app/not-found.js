a08ed5b5713ad94b48715e8a27163c0131c2cee6
syrnjs <ksy21019@gmail.com>
2025-05-20 10:33:22 +0900

    feat: 404 page 구현

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1 className="text-2xl text-red-500 font-bold">
+        404 - 페이지를 찾을 수 없습니다
+      </h1>
+    </div>
+  )
+}
